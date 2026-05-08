83d47d8fa324029b3830f4e564f247256d4d6056
marit.van.dijk <mlvandijk@gmail.com>
2026-05-08 12:47:05 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+<!-- Keep a Changelog guide -> https://keepachangelog.com -->
+
+# Polish-progress-bar Changelog
+
+## [Unreleased]
---
+++ src/main/kotlin/com/github/mlvandijk/polishprogressbar/startup/MyProjectActivity.kt
@@ -0,0 +1,16 @@
+package com.github.mlvandijk.polishprogressbar.startup
+
+import com.github.mlvandijk.polishprogressbar.progressbar.PolishFlagProgressIndicatorProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+
+/**
+ * Project activity that ensures the Polish Flag progress bar is initialized.
+ */
+class MyProjectActivity : ProjectActivity {
+
+    override suspend fun execute(project: Project) {
+        // Initialize the progress bar provider
+        PolishFlagProgressIndicatorProvider.getInstance()
+    }
+}
